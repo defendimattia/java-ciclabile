e540b7a0028ddecbf78998309f35fc999106fe35
defendimattia <defendi.mattia92@gmail.com>
2025-06-30 14:59:12 +0200

    Implemented App class with integer array management, getElementoSuccessivo() and hasAncoraElementi() methods

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
+
+    int[] arrayInteri;
+    int currentElement = 0;
+
+    public int getElementoSuccessivo() {
+        if (currentElement < arrayInteri.length) {
+            return arrayInteri[currentElement++];
+        }
+        return arrayInteri[currentElement - 1];
+    }
+
+    public boolean hasAncoraElementi() {
+
+        boolean hasElement = false;
+
+        if (currentElement < arrayInteri.length) {
+            hasElement = true;
+        }
+        return hasElement;
+    }
+
+    public App(int[] arrayInteri) {
+        this.arrayInteri = arrayInteri;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
     }
 }
